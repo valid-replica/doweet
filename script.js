4c793c8e5947971389e729743009cec8605d168a
Ervic Padilla <valid.replica@gmail.com>
2023-11-23 19:25:55 +0800

    Create html template

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.querySelector("#add-task-button").onclick = function () {
+  if (document.querySelector("#add-task").value.length === 0) {
+    alert("Please input a task");
+  } else {
+  }
+};
