94263ba3834ec9a24c8282b34601ec49f81738f0
Ervic Padilla <valid.replica@gmail.com>
2023-11-27 14:28:30 +0800

    Add localStorage and render todos in list

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 "use strict";
 
-document.querySelector("#add-task-button").onclick = function () {
-  if (document.querySelector("#add-task input").value.length === 0) {
+let todoInput = document.querySelector("#todo-input");
+let todoList = document.querySelector("#todo-list");
+let addTodo = document.querySelector("#add-task-button");
+
+addTodo.onclick = function () {
+  if (todoInput.value.length === 0) {
     alert("Please input a task");
   } else {
+    let todos = JSON.parse(localStorage.getItem("todos")) || [];
+    todos.push(todoInput.value);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    todoInput.value = "";
+    updateTodoList();
   }
 };
+
+function updateTodoList() {
+  todoList.innerHTML = "";
+
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos.forEach((todo, index) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
+    <li class="todo">
+            <span>${todo}</span>
+            <i class="fa-solid fa-trash trash" onclick="deleteTodo(${index})" aria-hidden="true"></i>
+    </li>
+    `;
+    todoList.appendChild(li);
+  });
+}
+
+function deleteTodo(index) {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos.splice(index, 1);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  updateTodoList();
+}
 
 let allButton = document.querySelector("#all-button");
 let todoButton = document.querySelector("#todo-button");
@@ -22,3 +58,5 @@ todoButton.onclick = function () {
 completedButton.onclick = function () {
   completedButton.style.cssText = `color: #181123; background-color: #fafafa; font-size: 26px;`;
 };
+
+updateTodoList();
